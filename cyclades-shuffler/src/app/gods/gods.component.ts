400efe3b8bffadce24ce455e34f7c66624ac8d45
lberrocal <luis.berrocal@cignium.com>
2018-05-20 15:45:17 -0500

    Gods component creaiton

---
+++ cyclades-shuffler/src/app/gods/gods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gods',
+  templateUrl: './gods.component.html',
+  styleUrls: ['./gods.component.css']
+})
+export class GodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
