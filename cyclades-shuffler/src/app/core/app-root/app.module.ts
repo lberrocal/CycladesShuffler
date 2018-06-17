8f6a778e6cbf1f14327c55c33fd912d1a966ca2e
lberrocal <luisberrocal@outlook.com>
2018-06-17 03:26:16 -0500

    Project Restructuring

--- cyclades-shuffler/src/app/core/app-root/app.module.ts
+++ cyclades-shuffler/src/app/core/app-root/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { GodsComponent } from './gods/gods.component';
+import { GodsComponent } from '../../modules/gods/gods.component';
 
 
 @NgModule({
